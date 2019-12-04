00dcc8dcc27da3d833170e02380c657235baba71
Chris Langsford <chrisl@discovery.co.za>
2019-12-04 06:38:22 +0200

    enhanced harness to read input from another file to de-clutter solutions

--- 4/day-4.js
+++ 4/day-4.js
@@ -1,6 +1,15 @@
+const input = require("./input-4");
 
-function part1(){}
-function part2(){}
+console.log(`${part1(input.string)}`);
+console.log(`${part2()}`);
+
+function part1(input) {
+    return 0;
+}
+
+function part2(input) {
+    return 0;
+}
 
 
 module.exports = {part1: part1, part2: part2};
