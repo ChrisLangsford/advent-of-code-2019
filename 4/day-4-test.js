1adecb1b2e0bd949e7601f8c910188e65a96dcce
Chris Langsford <chrisl@discovery.co.za>
2019-12-04 06:30:41 +0200

    Added harness for day 4

---
+++ 4/day-4-test.js
@@ -0,0 +1,15 @@
+let assert = require('assert');
+let day4 = require('./day-4.js');
+
+describe('Day 3 Part 1', () => {
+    describe('test 1', () => {
+        it('should return ', () => {
+            assert.equal(day4.part1(""), 0);
+        });
+    });
+    describe('test 2', () => {
+        it('should return ', () => {
+            assert.equal(day4.part1(""), 0);
+        });
+    });
+});
---
+++ 4/day-4.js
@@ -0,0 +1,6 @@
+
+function part1(){}
+function part2(){}
+
+
+module.exports = {part1: part1, part2: part2};
