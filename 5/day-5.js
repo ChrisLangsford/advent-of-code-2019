dee64d690676498f2a50845bb29b7f038991a2db
Chris Langsford <chrisl@discovery.co.za>
2019-12-06 05:56:16 +0200

    refactored copy past code out into get args method to make part 2 cleaner

--- 5/day-5.js
+++ 5/day-5.js
@@ -1,17 +1,31 @@
 const input = require("./input-5");
 
-console.log(`Part 1: ${part1(input.string)}`);
-console.log(`Part 2: ${part2(input.string)}`);
+console.log(`Part 1: ${part1(input.string, 1)}`);
+console.log(`Part 2: ${part2(input.string, 5)} do not trust me, The program runs for one cycle after the print instruction making the final print out incorrect`);
 
-function part1(input) {
-    return intCode(input.split(',').map(x => parseInt(x)));
+function part1(input, startCode) {
+    return intCode(input.split(',').map(x => parseInt(x)), startCode);
 }
 
-function part2(input) {
-    return null;
+function part2(input, startCode) {
+    return intCode(input.split(',').map(x => parseInt(x)), startCode);
 }
 
-function intCode(initialState) {
+function getArgs(P, ip, n, digits) {
+    while (digits.length < n) {
+        digits = [0, ...digits];
+    }
+    let i1 = P[ip + 1];
+    let i2 = P[ip + 2];
+
+    let p1 = digits[n - 1] === 1 ? i1 : P[i1];
+    let p2 = digits[n - 2] === 1 ? i2 : P[i2];
+
+    return {p1: p1, p2: p2}
+
+}
+
+function intCode(initialState, startCode) {
     let P = [...initialState];
     let ip = 0;
 
@@ -25,32 +39,28 @@ function intCode(initialState) {
         digits.pop();
         digits.pop();
 
+        let p = null;
+        let i1;
+        let i3 = P[ip + 3];
+
         switch (opCode) {
             case 1:
-                while (digits.length < 3) {
-                    digits = [0, ...digits];
-                }
-                i1 = P[ip + 1];
-                i2 = P[ip + 2];
-                i3 = P[ip + 3];
+                p = getArgs(P, ip, 3, digits);
 
-                P[i3] = (digits[2] === 1 ? i1 : P[i1]) + (digits[1] === 1 ? i2 : P[i2]);
+                P[i3] = p.p1 + p.p2;
+
                 ip += 4;
                 break;
             case 2:
-                while (digits.length < 3) {
-                    digits = [0, ...digits];
-                }
-                i1 = P[ip + 1];
-                i2 = P[ip + 2];
-                i3 = P[ip + 3];
+                p = getArgs(P, ip, 3, digits);
 
-                P[i3] = (digits[2] === 1 ? i1 : P[i1]) * (digits[1] === 1 ? i2 : P[i2]);
+                P[i3] = p.p1 * p.p2;
+
                 ip += 4;
                 break;
             case 3:
                 i1 = P[ip + 1];
-                P[i1] = 1;
+                P[i1] = startCode;
                 ip += 2;
                 break;
             case 4:
@@ -58,8 +68,41 @@ function intCode(initialState) {
                 console.log(P[i1]);
                 ip += 2;
                 break;
+            case 5:
+                p = getArgs(P, ip, 2, digits);
+
+                if (p.p1 !== 0) {
+                    ip = p.p2;
+                } else {
+                    ip += 3;
+                }
+                break;
+            case 6:
+                p = getArgs(P, ip, 2, digits);
+
+                if (p.p1 === 0) {
+                    ip = p.p2;
+                } else {
+                    ip += 3;
+                }
+                break;
+            case 7:
+                p = getArgs(P, ip, 3, digits);
+
+                P[i3] = p.p1 < p.p2 ? 1 : 0;
+
+                ip += 4;
+                break;
+            case 8:
+                p = getArgs(P, ip, 3, digits);
+
+                P[i3] = p.p1 === p.p2 ? 1 : 0;
+
+                ip += 4;
+                break;
             case 99:
-                return P[i1];
+                p = getArgs(P, ip, 1, digits);
+                return p.p1;
         }
 
     }
