dd324465682b1da94935cec2953c486e6a849532
Chris Langsford <chrisl@discovery.co.za>
2019-12-07 07:45:22 +0200

    set up day 7

---
+++ 7/day-7-test.js
@@ -0,0 +1,18 @@
+let assert = require('assert');
+let day7 = require('./day-7.js');
+
+describe('Day 7 Part 1', () => {
+    describe('test 1', () => {
+        it('should', () => {
+            assert.equal(day7.part1(), null);
+        });
+    });
+});
+
+describe('Day 6 Part 2', () => {
+    describe('test 1', () => {
+        it('should', () => {
+            assert.equal(day7.part2(""), null);
+        });
+    });
+});
---
+++ 7/day-7.js
@@ -0,0 +1,16 @@
+const input = require("./input-7");
+
+console.log(`Part 1: ${part1(input.string)}`);
+console.log(`Part 2: ${part2(input.string)}`);
+
+function part1(input) {
+}
+
+
+function part2(input) {
+}
+
+module.exports = {
+    part1: part1,
+    part2: part2
+};
