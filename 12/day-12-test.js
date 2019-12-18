52b5ececbced8adf840b921a0683d7fdaf07d960
Chris Langsford <chrisl@discovery.co.za>
2019-12-18 06:33:47 +0200

    set up for day 12

---
+++ 12/day-12-test.js
@@ -0,0 +1,23 @@
+let assert = require('assert');
+let day12 = require('./day-12.js');
+
+const input = {
+    i: {pos: [-1, 0, 2], vel: [0, 0, 0]},
+    e: {pos: [2, -10, -7], vel: [0, 0, 0]},
+    g: {pos: [4, -8, 8], vel: [0, 0, 0]},
+    c: {pos: [3, 5, -1], vel: [0, 0, 0]},
+};
+
+describe('Day 12 Part 1', () => {
+    describe('run tests', () => {
+        it('should ', () => {
+            assert.equal(true, true);
+        });
+    });
+
+});
+
+describe('Day 12 Part 2', () => {
+    describe('full test', () => {
+    });
+});
---
+++ 12/day-12.js
@@ -0,0 +1,23 @@
+const input = require("./input-12");
+
+console.log(`Part 1: ${part1(input)}`);
+console.log(`Part 2: ${part2(input)}`);
+
+function part1(input) {
+}
+
+
+function part2(input) {
+}
+
+function tick(input) {
+
+}
+
+
+module.exports = {
+    part1: part1,
+    part2: part2,
+    tick, tick
+};
+
