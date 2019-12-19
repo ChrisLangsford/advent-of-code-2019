00f6b5946a2036c2c4030dcf4539c8eb5f8184a9
Chris Langsford <chrisl@discovery.co.za>
2019-12-19 18:22:11 +0200

    completed day 12 with some assistance of an LCM function found on StackOverflow :P

--- 12/day-12.js
+++ 12/day-12.js
@@ -14,7 +14,7 @@ function part1(input, numberOfSteps) {
 function part2(input) {
     let numberOfSteps = runUntilRepeatFound(input);
     console.log(`after ${numberOfSteps} steps`);
-    return input;
+    return numberOfSteps;
 }
 
 function runForNumberOfTicks(input, numberOfTicks) {
@@ -25,20 +25,85 @@ function runForNumberOfTicks(input, numberOfTicks) {
 }
 
 function runUntilRepeatFound(input) {
-    let loop = true;
-    let inputs = [];
-    inputs.push(JSON.stringify(input));
-    while (loop) {
-        input = tick(input);
-        if (inputs.includes(JSON.stringify(input))) {
-            loop = false;
-        } else {
-            inputs.push(JSON.stringify(input));
-        }
-        console.log(`Steps: ${inputs.length}`);
-    }
+    //TODO: Non-brute force: find how long it takes for each dimension to repeat and take the LCM of those values
 
-    return inputs.length;
+    // let loop = true;
+    // let inputs = [];
+    // let count = 0;
+    // inputs.push(JSON.stringify(input));
+    // while (loop) {
+    //     input = tick(input);
+    //     count++;
+    //     if (inputs[0] === input) {
+    //         loop = false;
+    //     }
+    //     console.log(`Steps: ${count}`);
+    // }
+    //
+    // return inputs.length;
+    const initialState = JSON.parse(JSON.stringify(input));
+    let xSteps = 0;
+    let ySteps = 0;
+    let zSteps = 0;
+    let xFound = false;
+    let yFound = false;
+    let zFound = false;
+
+    while (!xFound || !yFound || !zFound) {
+        input = tick(input);
+        if (!xFound && comparePositionAndVelocity(input, initialState, 0)) {
+            xSteps++;
+            xFound = true;
+        } else if (!xFound) {
+            xSteps++;
+        }
+        if (!yFound && comparePositionAndVelocity(input, initialState, 1)) {
+            ySteps++;
+            yFound = true;
+        } else if (!yFound) {
+            ySteps++;
+        }
+        if (!zFound && comparePositionAndVelocity(input, initialState, 2)) {
+            zSteps++;
+            zFound = true;
+        } else if (!zFound) {
+            zSteps++;
+        }
+    }
+    console.log(`xSteps: ${xSteps}. ySteps: ${ySteps}. zSteps: ${zSteps}. `);
+    return LCM([xSteps, ySteps, zSteps]);
+
+}
+
+/**
+ * @return {number}
+ */
+function LCM(numbers) {
+    console.log(numbers);
+    if (numbers.length < 2) return;
+    first = numbers[0];
+    second = numbers[1];
+    var i = j = 1;
+    var mult1 = first * i++;
+    var mult2 = second * j++;
+    while (mult1 !== mult2) {
+        if (mult1 < mult2)
+            mult1 = first * i++;
+        else
+            mult2 = second * j++
+    }
+    if (numbers.length > 2) {
+        numbers[1] = mult1; //I hope you're fine with the fact that 'numbers' gets modified
+        mult1 = LCM(numbers.splice(1, numbers.length - 1))
+    }
+    return mult1;
+}
+
+function comparePositionAndVelocity(input, initial, plane) {
+    return (input.i.pos[plane] === initial.i.pos[plane] && input.i.vel[plane] === initial.i.vel[plane] &&
+        input.e.pos[plane] === initial.e.pos[plane] && input.e.vel[plane] === initial.e.vel[plane] &&
+        input.g.pos[plane] === initial.g.pos[plane] && input.g.vel[plane] === initial.g.vel[plane] &&
+        input.c.pos[plane] === initial.c.pos[plane] && input.c.vel[plane] === initial.c.vel[plane]);
 }
 
 function tick(input) {
