bba858c8181cbae9712f651302696749b1d3ee13
Chris Langsford <chrisl@discovery.co.za>
2019-12-19 19:05:47 +0200

    intcode day 2 complete

--- intCode/intCode.js
+++ intCode/intCode.js
@@ -1,3 +1,27 @@
-module.exports = intcode = function intCode(memory) {
-    return memory.split(',').map(x => parseInt(x));
+module.exports = intcode = function intCode(input) {
+    let memory = input.split(',').map(x => parseInt(x));
+    let ip = 0;
+    let run = true;
+
+    while (run) {
+        let opCode = memory[ip];
+        switch (opCode) {
+            case 1: {
+                memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
+                ip += 4;
+                break;
+            }
+            case 2: {
+                memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
+                ip += 4;
+                break;
+            }
+            case 99: {
+                run = false;
+                break;
+            }
+        }
+    }
+
+    return memory;
 };
