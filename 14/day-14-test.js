1a64ccad85c6c5eeea803c5d4878d97b9ed06f1c
Chris Langsford <chrisl@discovery.co.za>
2019-12-26 17:07:29 +0200

    Added tests from day 14 examples

--- 14/day-14-test.js
+++ 14/day-14-test.js
@@ -3,7 +3,35 @@ let day14 = require('./day-14');
 
 describe('Day 14 Part 1', () => {
     describe('run tests', () => {
-        it('should ', () => {
+        it('should return 31 for example 1', () => {
+            let input = "10 ORE => 10 A\n" +
+                "1 ORE => 1 B\n" +
+                "7 A, 1 B => 1 C\n" +
+                "7 A, 1 C => 1 D\n" +
+                "7 A, 1 D => 1 E\n" +
+                "7 A, 1 E => 1 FUEL";
+            assert.equal(day14.part1(input), 31)
+        });
+
+        it('should return 2210736 for the last example', () => {
+            const input = "171 ORE => 8 CNZTR\n" +
+                "7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL\n" +
+                "114 ORE => 4 BHXH\n" +
+                "14 VRPVC => 6 BMBT\n" +
+                "6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL\n" +
+                "6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT\n" +
+                "15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW\n" +
+                "13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW\n" +
+                "5 BMBT => 4 WPTQ\n" +
+                "189 ORE => 9 KTJDG\n" +
+                "1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP\n" +
+                "12 VRPVC, 27 CNZTR => 2 XDBXC\n" +
+                "15 KTJDG, 12 BHXH => 5 XCVML\n" +
+                "3 BHXH, 2 VRPVC => 7 MZWV\n" +
+                "121 ORE => 7 VRPVC\n" +
+                "7 XCVML => 6 RJRHP\n" +
+                "5 BHXH, 4 VRPVC => 5 LTCX";
+            assert.equal(day14.part1(input), 2210736 )
         });
     });
 
